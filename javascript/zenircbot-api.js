edaa529cfe8904d6430762c925661a86dbff6bdb
Chris McDonald <XWraithanX@gmail.com>
2012-04-13 11:30:03 -0700

    release of javascript api

--- javascript/zenircbot-api.js
+++ javascript/zenircbot-api.js
@@ -1,67 +1,67 @@
 var fs = require('fs');
 var redis_lib = require('redis');
-var pub = null;
 
 
-ZenIRCBot = function(host, port, db) {
-    this.host = host || 'localhost';
-    this.port = port || 6379;
-    this.db = db || 0;
+function ZenIRCBot(host, port, db) {
+    var self = this;
+    self.host = host || 'localhost';
+    self.port = port || 6379;
+    self.db = db || 0;
+    self.redis = self.get_redis_client();
 }
 
 ZenIRCBot.prototype.send_privmsg = function(to, message) {
-    if (!pub) {
-        pub = get_redis_client();
-    }
+    var self = this;
     if (typeof(to) == 'string') {
-        to = [to]
+        to = [to];
     }
     to.forEach(function(destination) {
-        pub.publish('out', JSON.stringify({
+        self.redis.publish('out', JSON.stringify({
             version: 1,
             type: 'privmsg',
             data: {
                 to: destination,
                 message: message
             }
-        }))
+        }));
     });
-}
+};
 
 ZenIRCBot.prototype.send_admin_message = function(message) {
-    var config = load_config('../bot.json');
-    send_privmsg(config.servers[0].admin_spew_channels, message);
-}
+    var self = this;
+    self.redis.get('zenircbot:admin_spew_channels', function(err, channels) {
+        self.send_privmsg(channels, message);
+    });
+};
 
 ZenIRCBot.prototype.register_commands = function(service, commands) {
-    send_admin_message(service + ' online!')
-    sub = get_redis_client();
+    var self = this;
+    self.send_admin_message(service + ' online!');
+    var sub = self.get_redis_client();
     sub.subscribe('in');
     sub.on('message', function(channel, message){
-        msg = JSON.parse(message)
+        var msg = JSON.parse(message);
         if (msg.version == 1 && msg.type == 'privmsg') {
             if (msg.data.message == 'commands') {
                 commands.forEach( function(command, index) {
-                    send_privmsg(msg.data.sender,
-                                 service + ': ' +
-                                 command.name + ' - ' +
-                                 command.description);
+                    self.send_privmsg(msg.data.sender,
+                                      service + ': ' +
+                                      command.name + ' - ' +
+                                      command.description);
                 });
             }
         }
     });
-    return sub
-}
+    return sub;
+};
 
-ZenIRCBot.prototype.get_redis_client = function(redis_config) {
-    if (!redis_config) {
-        redis_config = load_config('../bot.json').redis;
-    }
-    return redis_lib.createClient(redis_config.port,
-                                  redis_config.host, {
-                                      selected_db: redis_config.db
+ZenIRCBot.prototype.get_redis_client = function() {
+    var self = this;
+    return redis_lib.createClient(self.port,
+                                  self.host, {
+                                      selected_db: self.db
                                   });
-}
+};
 
 function load_config(name) {
     return JSON.parse(fs.readFileSync(name, 'utf8'));
